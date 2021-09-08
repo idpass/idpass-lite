432c2f79b538ddfde60c749d6b9e787e768eaa26
dexter <dexter@newlogic.com>
2021-09-08 10:22:06 +0800

    Add proper macos m1 build flags for libsodium build script

--- dependencies/scripts/build.libsodium.macosm1.sh
+++ dependencies/scripts/build.libsodium.macosm1.sh
@@ -4,6 +4,12 @@
 export ABI=macosm1
 export INSTALL_PREFIX=$project/dependencies/build/$ABI
 
+export LIBSODIUM_ENABLE_MINIMAL_FLAG="--enable-minimal"
+export MACOS_ARM64_PREFIX="${INSTALL_PREFIX}"
+export MACOS_VERSION_MIN=10.10
+export CFLAGS="-O2 -arch arm64 -mmacosx-version-min=${MACOS_VERSION_MIN}"
+export LDFLAGS="-arch arm64 -mmacosx-version-min=${MACOS_VERSION_MIN}"
+
 d=$(dirname $0)
 cd $d
 
@@ -12,13 +18,11 @@ cd $d
 make clean
 make distclean
 
-export CFLAGS="-Os"
-
 ./configure \
-    --host=arm-none-eabi \
+    --host=arm-apple-darwin20 \
     --disable-shared \
-    --with-pic="yes" \
-    --prefix="$INSTALL_PREFIX"
+    --prefix="$INSTALL_PREFIX" \
+    ${LIBSODIUM_ENABLE_MINIMAL_FLAG}
 
 make
 make install
